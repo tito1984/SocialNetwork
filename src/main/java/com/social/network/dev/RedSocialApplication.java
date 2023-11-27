fa56106a6708bcb9d4c729174e1ead491d39da96
tito1984 <txelan84@gmail.com>
2023-11-27 15:28:58 +0100

    First commit

---
+++ src/main/java/com/social/network/dev/RedSocialApplication.java
@@ -0,0 +1,13 @@
+package com.social.network.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedSocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/social/network/dev/RedSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.social.network.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
